1fe82b974aef9137e77e7e6243f8d908ad28b596
e.gaifutdinov <e.gaifutdinov@wellyes.ru>
2020-11-06 13:37:47 +0300

    add new task

---
+++ Typos/Typos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Typos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // Фигурные скобки + апострафы
+            var number = 5.5; // Запятая для разделение целого и вещественного недопустима. Ошибка в имени переменной
+            number += 7; 
+            Console.WriteLine(number); // Ошибка в имени метода, пропущен кэмелКейс
+        }
+    }
+}
